c99b6214c707983c2491f326a28f8772b9c00bed
Michael Banuelos <mbanuelos1995@gmail.com>
2021-02-24 12:15:15 -0800

    setting up get request

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+import API_KEY from '../../../config.js'
 
-let App = () => (
-  <div>hello</div>
-)
+let App = () => {
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hrlax42/', {
+    headers: {
+      'Authorization': `Basic ${API_KEY}`
+    }
+  }).then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  return (
+    <div>React</div>
+  )
+}
 
 export default App;
