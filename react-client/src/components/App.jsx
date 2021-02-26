ae37dec14a8ac3c64f2a8f91987065e293c85779
Michael Banuelos <mbanuelos1995@gmail.com>
2021-02-25 19:33:06 -0800

    turned app into a stateful function. wrote some helper functions to do API requests.

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -2,18 +2,55 @@ import React from 'react';
 import axios from 'axios';
 import header from '../../../config.js'
 
-let App = () => {
-  console.log(header)
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products', header)
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-  return (
-    <div>React</div>
-  )
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allProducts: [],
+      selectedItemIndex: 0,
+      relatedItems: []
+    }
+    this.getProducts = this.getProducts.bind(this);
+    this.selectProduct = this.selectProduct.bind(this);
+    this.getRelatedItems = this.getRelatedItems.bind(this);
+  }
+  componentDidMount() {
+    this.getProducts();
+  }
 
-export default App;
+  selectProduct(index) {
+    this.setState({
+      selectedItemIndex: index
+    })
+  }
+
+  getRelatedItems() {
+    let id = this.state.allProducts[this.state.selectedItemIndex].id
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${id}/related`, header)
+      .then((data) => {
+        this.setState({
+          relatedItems: data.data
+        }, () => console.log(this.state))
+      })
+      .catch((err) => console.log(err))
+  }
+
+  getProducts() {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?count=10&page=5', header)
+      .then((data) => {
+        this.setState({
+          allProducts: data.data
+        })
+      })
+      .then(() => this.getRelatedItems())
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>React</div>
+    )
+  }
+}
