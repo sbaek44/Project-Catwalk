182a81b8fe2a9ec7cbd0d581d22e053423d2bc44
Kun <kunvalentinekim@gmail.com>
2021-03-02 17:22:35 -0800

    implemented add questions and answers

---
+++ react-client/src/components/Questions-Answers/AnswerModals.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import header from '../../../../config.js';
+
+
+const AnswerModals = (props) => {
+  const [answer, setAnswer] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [email, setEmail] = useState('');
+  const [photos, setPhotos] = useState([])
+  const [photoClicked, setClicked] = useState(false)
+  const [imgPreview, setImgPreview] = useState([])
+
+
+
+  useEffect( ()=> {
+    imageDisplay()
+    console.log(photos)
+
+  },[photos,imgPreview])
+
+  let addAnswer = () => {
+    let id = props.question.question_id
+      axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions/${id}/answers`,{
+        body: answer,
+        name: nickname,
+        email: email,
+        photos: photos
+      }, header)
+      .then(() => {console.log('success')})
+      .catch( (err)=> {console.log('error')})
+
+  };
+
+
+  let validEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLowerCase());
+  }
+
+
+  let submitHandler = (e) => {
+    e.preventDefault()
+   let error = []
+    if (answer.length === 0) {error.push("You must enter the following: Question")}
+    if (nickname.length === 0) {if (error.length===0){error.push("You must enter the following: Name")} else{error.push(", Name")}}
+    if (email.length === 0) {if (error.length===0){error.push("You must enter the following: Email")} else{error.push(", Email")}}
+    else if (!validEmail(email)) {if(error.length ===0) {error.push('The email address provided is not in correct email format')} else{error.push(', and The email address provided is not in correct email format')}}
+
+
+    if(error.length===0) {addAnswer()}
+    else {alert(error.join(''))}
+  }
+
+  let imageDisplay = ()=> {
+    if(photos.length > 4) {
+      return (
+        <div>
+          {imgPreview.map((image)=> {
+            return <img src={image} alt="" key={image}/>
+          })}
+      </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <input type='file' onChange={getPhoto}/>
+          {imgPreview.map((image)=> {
+            return <img className='img' src={image} alt="" key={image}/>
+          })}
+        </div>
+      )
+    }
+  }
+
+  let getPhoto = (e) => {
+    e.preventDefault();
+    let reader = new FileReader();
+    let img = e.target.files[0];
+
+    reader.onloadend = () => {
+      setPhotos([...photos, img])
+      setImgPreview([...imgPreview, reader.result])
+    }
+
+    if(img) {reader.readAsDataURL(img);}
+  }
+
+  return (
+    <div>
+       <div className = "AnswerModal">
+        <h1>Sumbit your answer</h1> <h3>{props.question.question_body}:{props.product.name}</h3>
+        <form onSubmit ={submitHandler}>
+
+          <label>Your Answer *
+          <input type="text" onChange= {e=> setAnswer(e.target.value)} value= {answer}/>>
+          </label>
+
+
+          <label>What is your nickname? *
+          <input type="text" placeholder="Example: jack543!" onChange= {e=> setNickname(e.target.value)} value = {nickname}/>
+          For privacy reasons, do not use your full name or email address
+          </label>
+
+          <label>Your Email *
+            <input type="text" placeholder="Example: jack@email.com" onChange= {e=> setEmail(e.target.value)} value={email}/>
+            For authentication reasons, you will not be emailed
+          </label>
+
+          <input type="submit" value = "Submit"/>
+
+
+          {imageDisplay()}
+
+          {/* {!photoClicked ? <div></div>} */}
+
+
+        </form>
+        </div>
+    </div>
+      )
+};
+
+export default AnswerModals;
---
+++ react-client/src/components/Questions-Answers/QuestionModals.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import header from '../../../../config.js';
+
+
+const QuestionModals = (props) => {
+  const [addClick, addClicked] = useState(false)
+  const [question, setQuestion] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [email, setEmail] = useState('')
+
+
+  useEffect( ()=> {
+
+  },[])
+
+  let addQuestions = () => {
+    let id = props.product.id
+      axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions`,{
+        body: question,
+        name: nickname,
+        email: email,
+        product_id: props.product.id
+      }, header)
+      .then(() => {console.log('success')})
+      .catch( (err)=> {console.log('error')})
+
+  };
+
+
+  let validEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLowerCase());
+  }
+
+
+  let submitHandler = (e) => {
+    e.preventDefault()
+   let error = []
+    if (question.length === 0) {error.push("You must enter the following: Question")}
+    if (nickname.length === 0) {if (error.length===0){error.push("You must enter the following: Name")} else{error.push(", Name")}}
+    if (email.length === 0) {if (error.length===0){error.push("You must enter the following: Email")} else{error.push(", Email")}}
+    else if (!validEmail(email)) {if(error.length ===0) {error.push('The email address provided is not in correct email format')} else{error.push(', and The email address provided is not in correct email format')}}
+
+
+    if(error.length===0) {addQuestions()}
+    else {alert(error.join(''))}
+  }
+
+  return (
+    <div>
+      <button onClick={()=>{addClicked(!addClick)}}>ADD A QUESTION +</button>
+      {!addClick ? null
+      : <div className = "QuestionModal">
+        <h1>Ask Your Question</h1> <h3>About the {props.product.name}</h3>
+        <form onSubmit ={submitHandler}>
+
+          <label>Your Question *
+          <input type="text" onChange= {e=> setQuestion(e.target.value)} value= {question}></input>
+          </label>
+
+
+          <label>What is your nickname?*
+          <input type="text" placeholder="Example: jackson11!" onChange= {e=> setNickname(e.target.value)} value = {nickname}></input>
+          For privacy reasons, do not use your full name or email address
+          </label>
+
+          <label>Your Email*
+            <input type="text" placeholder="Why did you like the product or not?" onChange= {e=> setEmail(e.target.value)} value={email}></input>
+            For authentication reasons, you will not be emailed
+          </label>
+
+          <input type="submit" value = "Submit"></input>
+
+
+        </form>
+        </div>
+        }
+    </div>
+      )
+};
+
+export default QuestionModals;
