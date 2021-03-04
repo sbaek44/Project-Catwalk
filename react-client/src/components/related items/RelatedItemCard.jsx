76a6032fc362776b2bd6f0db779f205cecc12316
Kun <kunvalentinekim@gmail.com>
2021-03-04 15:56:57 -0800

    Fixed search questions

--- react-client/src/components/related items/RelatedItemCard.jsx
+++ react-client/src/components/related items/RelatedItemCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Modal from'react-modal';
+import Modal from 'react-modal';
 import axios from 'axios';
 import header from '../../../../config.js';
 
