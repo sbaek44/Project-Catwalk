fa1afab1fb37fb297b9d8bab3f51b4320f9d2b3a
Jake Carr <jcarr0157@gmail.com>
2021-03-04 18:41:18 -0800

    expanded view functionality coming together. not 100% behaving as business docs describe but the core pieces are there. had to add event.stopPropagation all over the place to deal with different onClicks that are nested on top of each other in image gallery component

--- react-client/src/components/product-overview/ExpandedView.jsx
+++ react-client/src/components/product-overview/ExpandedView.jsx
@@ -3,6 +3,8 @@ import PrismaZoom from 'react-prismazoom'
 
 export default function ExpandedView({ close, handleIconClick, url, photos, back, forward, selectedPhotoIndex }) {
 
+  const [isZoomed, toggleZoom] = useState(false);
+
   const prismaZoom = createRef();
 
   const onClickOnZoomOut = () => {
@@ -19,7 +21,7 @@ export default function ExpandedView({ close, handleIconClick, url, photos, back
         return <div
           className='expanded-view-icon'
           key={i}
-          onClick={() => handleIconClick(photo.url, i)}
+          onClick={(e) => handleIconClick(e, photo.url, i)}
           id={i === selectedPhotoIndex ? 'selected' : null}
         >
         </div>
@@ -28,21 +30,20 @@ export default function ExpandedView({ close, handleIconClick, url, photos, back
   }
 
   return (
-    <div>
-      <div>
-        <button onClick={() => close()} style={{ width: 20, height: 20 }}>X</button>
-        <div className='expanded-arrow-and-icon-container'>
-          <button className='expanded-arrow-button' onClick={back()}>&#x2190;</button>
-          {renderExpandedViewIcons()}
-          <button className='expanded-arrow-button' onClick={forward()}>&#x2192;</button>
-        </div>
-        <PrismaZoom
-          maxZoom={2.5}
-          ref={prismaZoom}
-        >
-          <img className='expanded-view-image' src={url} />
-        </PrismaZoom>
+    <div className='expanded-gallery-modal-inner'>      <button onClick={() => close()} id='modal-x-button'>&#x2715;</button>
+      <div className='expanded-arrow-and-icon-container'>
+        <div className='arrow-container'>{selectedPhotoIndex > 0 ? <button className='arrow-button' onClick={(e) => {back(e)}}>&#x2190;</button> : null}</div>
+        {renderExpandedViewIcons()}
+        <div className='arrow-container'>{selectedPhotoIndex < photos.length - 1? <button className='arrow-button' onClick={(e) => {forward(e)}}>&#x2192;</button> : null}</div>
       </div>
+      <PrismaZoom
+        onZoomChange={() => toggleZoom(!isZoomed)}
+        minZoom={1}
+        maxZoom={2.5}
+        ref={prismaZoom}
+      >
+        <img className='expanded-view-image' id={isZoomed ? 'zoomed' : 'not-zoomed'} src={url} />
+      </PrismaZoom>
     </div>
   )
 }
