9717ad7ee205304cad810924e90348fd086d5940
Jake Carr <jcarr0157@gmail.com>
2021-03-12 09:53:00 -0800

    fixed disabled prop on react select tags, it is isDisabled not just disabled. installed react-select-event testing library

--- react-client/src/components/product-overview/AddToCart.jsx
+++ react-client/src/components/product-overview/AddToCart.jsx
@@ -107,7 +107,7 @@ function AddToCart({
         .catch((err) => {
           console.error(err);
         });
-      alert(`Added ${qty}${qty > 1 ? 'x' : '' } size ${size} ${product.name} in ${getStyleName()} to cart!`);
+      alert(`Added ${qty}${qty > 1 ? 'x' : ''} size ${size} ${product.name} in ${getStyleName()} to cart!`);
     }
   };
 
@@ -191,10 +191,9 @@ function AddToCart({
                 onFocus={() => toggleSizeMenu(true)}
                 blurInputOnSelect
                 onChange={handleSizeSelect}
-                value={[{ value: size !== '' ? size : 'SELECT SIZE', label: size !== '' ? size : 'SELECT SIZE' }]}
-                disabled={outOfStock ? true : false}
+                value={[{ value: size !== '' ? size : outOfStock ? 'OUT OF STOCK' : 'SELECT SIZE', label: size !== '' ? size : outOfStock ? 'OUT OF STOCK' : 'SELECT SIZE' }]}
+                isDisabled={outOfStock}
                 options={sizeOptions}
-                placeholder={outOfStock ? 'OUT OF STOCK' : 'SELECT SIZE'}
                 menuIsOpen={sizeMenuOpen}
                 isSearchable={false}
               />
@@ -239,7 +238,7 @@ function AddToCart({
                 blurInputOnSelect
                 onChange={handleQtySelect}
                 value={[{ value: size !== '' ? qty : '-', label: size !== '' ? qty : '-' }]}
-                disabled={size === '' ? true : false}
+                isDisabled={size === '' ? true : false}
                 options={qtyOptions}
               />
             </div>
@@ -260,8 +259,8 @@ function AddToCart({
               <button
                 widgetname="overview"
                 type="button"
-                className={darkMode ? "favorite-dark" :"favorite-button"}
-                onClick={() => alert(`FAVORITED ${product.name} !`)}
+                className={darkMode ? "favorite-dark" : "favorite-button"}
+                onClick={() => alert(`FAVORITED ${product.name}!`)}
               >
                 ☆
               </button>
